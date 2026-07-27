083d334956786f37bcc601878500fe98c35a3156
Kanak Raj <31663313+kanakraj@users.noreply.github.com>
2026-07-27 17:04:19 -0400

    Add Terraform configuration for AWS instance

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "github_runner" {
+  ami           = "ami-0c02fb55956c7d316" 
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "github-actions-runner"
+  }
+}
